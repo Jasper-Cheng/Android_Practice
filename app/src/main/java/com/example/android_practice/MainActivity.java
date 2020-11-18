7bc62caf66bb7980ff88cefd0ea52ec4f8e09769
Jasper.Cheng <Jasper.Cheng@tymphany.com>
2020-11-18 10:57:37 +0800

    -fold_notification

--- app/src/main/java/com/example/android_practice/MainActivity.java
+++ app/src/main/java/com/example/android_practice/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RemoteViews;
 
 public class MainActivity extends AppCompatActivity {
     Button normal_button;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     String name = "我是渠道名字";
     PendingIntent pendingIntent = null;
     Intent mIntent = null;
+    RemoteViews remoteViews=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://blog.csdn.net/itachi85/"));
         pendingIntent = PendingIntent.getActivity(this, 0, mIntent, 0);
+        remoteViews=new RemoteViews(getPackageName(),R.layout.view_fold);
 
         normal_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,24 +47,52 @@ public class MainActivity extends AppCompatActivity {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     NotificationChannel mChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_LOW);
                     notificationManager.createNotificationChannel(mChannel);
-                    notification = new Notification.Builder(MainActivity.this)
+                    notification = new Notification.Builder(MainActivity.this,"normal8.0up")
                             .setContentIntent(pendingIntent)
                             .setAutoCancel(true)
                             .setChannelId(id)
-                            .setContentTitle("普通通知")
+                            .setContentTitle("普通通知android8.0以上")
                             .setContentText("My name is Jasper!")
                             .setSmallIcon(R.mipmap.ic_launcher).build();
                 } else {
-                    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this)
+                    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this,"normal8.0down")
                             .setContentIntent(pendingIntent)
                             .setAutoCancel(true)
-                            .setContentTitle("普通通知")
-                            .setContentText("My name is Jasper!")
+                            .setContentTitle("普通通知android8.0以下")
                             .setSmallIcon(R.mipmap.ic_launcher)
                             .setOngoing(true);
                     notification = notificationBuilder.build();
                 }
                 notificationManager.notify(111123, notification);
+            }
+        });
+
+        fold_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel mChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_LOW);
+                    notificationManager.createNotificationChannel(mChannel);
+                    notification = new Notification.Builder(MainActivity.this,"fold8.0up")
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
+                            .setChannelId(id)
+                            .setCustomBigContentView(remoteViews)
+                            .setContentTitle("折叠式通知android8.0以上")
+                            .setContentText("My name is Jasper!")
+                            .setSmallIcon(R.mipmap.ic_launcher).build();
+                } else {
+                    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this,"fold8.0down")
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
+                            .setContentTitle("折叠式通知android8.0以下")
+                            .setContentText("My name is Jasper!")
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setOngoing(true);
+                    notification = notificationBuilder.build();
+                    notificationBuilder.setCustomBigContentView(remoteViews);
+                }
+                notificationManager.notify(111124, notification);
             }
         });
     }
