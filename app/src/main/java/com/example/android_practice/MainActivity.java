77d1cfaed0da800d552ea9c7adba40f32aef61bc
Jasper.Cheng <Jasper.Cheng@tymphany.com>
2020-12-02 10:39:14 +0800

    -handle not_ask option

--- app/src/main/java/com/example/android_practice/MainActivity.java
+++ app/src/main/java/com/example/android_practice/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -51,7 +53,16 @@ public class MainActivity extends AppCompatActivity {
             if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
                 callPhone();
             }else{
-                Toast.makeText(this,"权限被拒绝",Toast.LENGTH_LONG).show();
+                if(!ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CALL_PHONE)){
+                    AlertDialog dialog=new AlertDialog.Builder(this).setMessage("该功能需要访问电话的权限，不开启将无法正常工作！")
+                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                  Toast.makeText(MainActivity.this,"如果你需要重新打开此权限，请去设置、应用权限中打开",Toast.LENGTH_LONG).show();
+                                }
+                            }).create();
+                    dialog.show();
+                }
             }
             return;
         }
