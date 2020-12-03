2ccb62abba5f8ffc2e53da6e73152734658a66e6
Jasper.Cheng <Jasper.Cheng@tymphany.com>
2020-12-03 10:29:53 +0800

    -snackbarEasyToUse

--- app/src/main/java/com/example/android_practice/MainActivity.java
+++ app/src/main/java/com/example/android_practice/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.android_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button snackbarButton;
+    private ConstraintLayout activity_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity_main=findViewById(R.id.activity_main);
+        snackbarButton=findViewById(R.id.snackbarButton);
+        snackbarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSnackbar();
+            }
+        });
+    }
+    public void showSnackbar(){
+        Snackbar.make(activity_main,"snackbar",Snackbar.LENGTH_LONG)
+                .setAction("点击事件", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Toast",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .show();
     }
 }
