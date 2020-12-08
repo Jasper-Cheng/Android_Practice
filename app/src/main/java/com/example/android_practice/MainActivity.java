2503413c07146a948bcee4fdef3f86a1822bb824
Jasper.Cheng <Jasper.Cheng@tymphany.com>
2020-12-08 11:19:06 +0800

    -use NavigationViewe implementation drawe menu layout

--- app/src/main/java/com/example/android_practice/MainActivity.java
+++ app/src/main/java/com/example/android_practice/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.android_practice;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
@@ -28,7 +34,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final ActionBar ab=getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        ab.setDisplayHomeAsUpEnabled(true);
         mViewPager=findViewById(R.id.viewpager);
+        mDrawerLayout=findViewById(R.id.dl_main_drawer);
+        NavigationView navigationView=findViewById(R.id.nv_main_navigation);
+        if (navigationView!=null){
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    menuItem.setChecked(true);
+                    String title=menuItem.getTitle().toString();
+                    Toast.makeText(getApplicationContext(),title,Toast.LENGTH_LONG).show();
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                }
+            });
+        }
         initViewPager();
     }
 
@@ -50,5 +73,15 @@ public class MainActivity extends AppCompatActivity {
         FragmentAdapter fragmentAdapter=new FragmentAdapter(getSupportFragmentManager(),fragments,titles);
         mViewPager.setAdapter(fragmentAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       switch (item.getItemId()){
+           case android.R.id.home:
+               mDrawerLayout.openDrawer(GravityCompat.START);
+               return true;
+       }
+       return super.onOptionsItemSelected(item);
     }
 }
