37f1ff35c0bdfd82123e1ea0184dc47c377ef980
Jasper.Cheng <Jasper.Cheng@tymphany.com>
2020-12-15 10:39:00 +0800

    -custom_view_anim_method

--- app/src/main/java/com/example/android_practice/MainActivity.java
+++ app/src/main/java/com/example/android_practice/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.android_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
+    private View customView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        customView=findViewById(R.id.customView);
+//        customView.setAnimation(AnimationUtils.loadAnimation(this,R.anim.translate));
+        ObjectAnimator.ofFloat(customView,"translationX",0,300).setDuration(1000).start();
     }
 }
