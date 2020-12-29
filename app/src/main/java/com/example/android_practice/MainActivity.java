2587a5c5f92d997722901aa248554744d9c3612b
Jasper.Cheng <Jasper.Cheng@tymphany.com>
2020-12-29 09:43:35 +0800

    custom_view_ViewGroup

--- app/src/main/java/com/example/android_practice/MainActivity.java
+++ app/src/main/java/com/example/android_practice/MainActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import view.TitleBar;
 
 public class MainActivity extends AppCompatActivity {
     private TitleBar titleBar;
+    private ListView lv_one;
+    private ListView lv_two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         titleBar = findViewById(R.id.titleBar);
+        lv_one = findViewById(R.id.lv_one);
+        lv_two = findViewById(R.id.lv_two);
         titleBar.setTitle("自定义组合控件啊");
         titleBar.setLeftListener(new View.OnClickListener() {
             @Override
@@ -29,5 +35,11 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "点击右键", Toast.LENGTH_SHORT).show();
             }
         });
+        String[] str1 = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, str1);
+        lv_one.setAdapter(adapter1);
+        String[] str2 = {"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, str2);
+        lv_two.setAdapter(adapter2);
     }
 }
---
+++ app/src/main/java/view/HorizontalView.java
@@ -0,0 +1,178 @@
+package view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+public class HorizontalView extends ViewGroup {
+    int lastInterceptX = 0;
+    int lastInterceptY = 0;
+    int lastX;
+    int lastY;
+    int currentIndex = 0;
+    int childWidth = 0;
+    private Scroller scroller;
+    private VelocityTracker tracker;
+
+    public HorizontalView(Context context) {
+        super(context);
+        init();
+    }
+
+    public HorizontalView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public HorizontalView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void init() {
+        scroller = new Scroller(getContext());
+        tracker = VelocityTracker.obtain();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercept = false;
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                intercept = false;
+                if (!scroller.isFinished()) {
+                    scroller.abortAnimation();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - lastInterceptX;
+                int deltaY = y - lastInterceptY;
+                if (Math.abs(deltaX) - Math.abs(deltaY) > 0) {
+                    intercept = true;
+                    Log.e("Jasper", "intercept=true");
+                } else {
+                    intercept = false;
+                    Log.e("Jasper", "intercept=false");
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                intercept = false;
+                break;
+        }
+        lastX = x;
+        lastY = y;
+        lastInterceptX = x;
+        lastInterceptY = y;
+        return intercept;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        tracker.addMovement(event);
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (!scroller.isFinished()) {
+                    scroller.abortAnimation();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - lastX;
+                scrollBy(-deltaX, 0);
+                break;
+            case MotionEvent.ACTION_UP:
+                int distance = getScrollX() - currentIndex * childWidth;
+                if (Math.abs(distance) > childWidth / 2) {
+                    Log.e("Jasper", "distance=" + distance);
+                    if (distance > 0) {
+                        currentIndex++;
+                    } else {
+                        currentIndex--;
+                    }
+                } else {
+                    tracker.computeCurrentVelocity(1000);
+                    float xV = tracker.getXVelocity();
+                    if (Math.abs(xV) > 50) {
+                        if (xV > 0) {
+                            currentIndex--;
+                        } else {
+                            currentIndex++;
+                        }
+                    }
+                }
+                currentIndex = currentIndex < 0 ? 0 : currentIndex > getChildCount() - 1 ? getChildCount() - 1 : currentIndex;
+                Log.e("Jasper", "currentIndex=" + currentIndex);
+                Log.e("Jasper", "childWidth=" + childWidth);
+                smoothScroll(currentIndex * childWidth, 0);
+                tracker.clear();
+                break;
+            default:
+                break;
+        }
+        lastX = x;
+        lastY = y;
+        return true;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        if (getChildCount() == 0) {
+            setMeasuredDimension(0, 0);
+        } else if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
+            View childOne = getChildAt(0);
+            int childWidth = childOne.getMeasuredWidth();
+            int childHeight = childOne.getMeasuredHeight();
+            setMeasuredDimension(childWidth * getChildCount(), childHeight);
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            int childWidth = getChildAt(0).getMeasuredWidth();
+            setMeasuredDimension(childWidth * getChildCount(), heightSize);
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            int childHeight = getChildAt(0).getMeasuredHeight();
+            setMeasuredDimension(widthSize, childHeight);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childCount = getChildCount();
+        int left = 0;
+        View child;
+        for (int i = 0; i < childCount; i++) {
+            child = getChildAt(i);
+            if (child.getVisibility() != View.GONE) {
+                int width = child.getMeasuredWidth();
+                childWidth = width;
+                child.layout(left, 0, left + width, child.getMeasuredHeight());
+                left += width;
+            }
+        }
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (scroller.computeScrollOffset()) {
+            scrollTo(scroller.getCurrX(), scroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    public void smoothScroll(int destX, int destY) {
+        scroller.startScroll(getScrollX(), getScrollY(), destX - getScrollX(), destY - getScrollY(), 1000);
+        invalidate();
+    }
+}
