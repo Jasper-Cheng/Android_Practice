7a1580d3b1f8c0f3ef8b2f190c3693fe3a0fe132
Jasper.Cheng <Jasper.Cheng@tymphany.com>
2021-01-27 13:42:22 +0800

    Volley_NetworkImageView

--- app/src/main/java/com/example/android_practice/MainActivity.java
+++ app/src/main/java/com/example/android_practice/MainActivity.java
@@ -2,34 +2,34 @@ package com.example.android_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button mImageLoader = null;
-    private ImageView content = null;
+    private Button NetWorkImageView = null;
+    private NetworkImageView content = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mImageLoader = findViewById(R.id.ImageLoader);
+        NetWorkImageView = findViewById(R.id.NetWorkImageView);
         content = findViewById(R.id.content);
-        mImageLoader.setOnClickListener(new View.OnClickListener() {
+        NetWorkImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RequestQueue mRequestQueue = Volley.newRequestQueue(getApplicationContext());
                 ImageLoader imageLoader = new ImageLoader(mRequestQueue, new BitmapCache());
-                ImageLoader.ImageListener listener = ImageLoader.getImageListener(content, R.mipmap.ic_launcher, R.mipmap.ic_launcher);
-                imageLoader.get("https://uploads-ssl.webflow.com/5b651f8b5fc94c6f5f470a7a/5cc68f1cd9da337009847c17_Volley%20-%20Open%20Graph%20Image.png", listener);
+                content.setImageUrl("https://uploads-ssl.webflow.com/5b651f8b5fc94c6f5f470a7a/5cc68f1cd9da337009847c17_Volley%20-%20Open%20Graph%20Image.png", imageLoader);
+                content.setDefaultImageResId(R.mipmap.ic_launcher);
+                content.setErrorImageResId(R.mipmap.ic_launcher);
             }
         });
     }
